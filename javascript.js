5e9d33e39ab3ac39dced6d4aff31209673bdd3d6
Turtle <braydenlynnpeterson@gmail.com>
2025-11-30 21:06:03 -0700

    Pagination for deans
    JS Requirement 1

---
+++ javascript.js
@@ -0,0 +1,41 @@
+// Deans page stuff
+const deans_deansPerPage = 4;
+const deans_container = document.getElementById("dean-list")
+const deans_pagination = document.getElementById("deans-paginate");
+const deans_pageLinks = document.querySelectorAll(".deans-pagination-link");
+const deans_deanCards = Array.from(deans_container.getElementsByClassName("deanlist-item"));
+const deans_totalPages = Math.ceil(deans_deanCards / deans_deansPerPage);
+let deans_currentPage = 1;
+
+function deans_displayPage(pageNum) {
+    const pageStart = (pageNum - 1) * deans_deansPerPage;
+    const pageEnd = pageStart + deans_deansPerPage;
+    deans_deanCards.forEach((deanItem, i) => {
+        if (i >= pageStart && i < pageEnd) {
+            deanItem.style.display = "block";
+        }
+        else {
+            deanItem.style.display = "none";
+        }
+    })
+    deans_pageLinks.forEach((link, i) => {
+        if (i + 1 == pageNum) {
+            link.style.fontSize = "1.25em";
+        }
+        else {
+            link.style.fontSize = "1em";
+        }
+    })
+}
+
+deans_pageLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const selectedPage = parseInt(link.getAttribute("data-pageNum"));
+        deans_currentPage = selectedPage;
+        deans_displayPage(deans_currentPage);
+        
+    })
+})
+deans_displayPage(1)
+// End of deans page
